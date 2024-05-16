ab7cf723d6227802ead4454f20bf211b35571ef0
Emily Alvim <105649387+snoozleEmily@users.noreply.github.com>
2024-05-16 18:29:26 -0300

    Changed the Presentation Text

--- src/data.js
+++ src/data.js
@@ -2,48 +2,24 @@
 
 const intros = [
   {
-    title: `Hello, I am Emily, and I am currently learning how to become a front-end developer.`, 
-    
-    presentation:`I discovered my fascination with coding in 2022, and I've been exploring the creative possibilities ever since. I am attending college to further develop my skills and knowledge in this field.
+    title: `Hello, I'm Emily and I aspire to become a Data Scientist`,
 
-      In addition to my passion for coding, I am also an English teacher. I enjoy teaching and helping others learn, and I believe this skill set will be valuable in my future as a developer. Clear communication is essential in both teaching and programming, and I strive to make my code easy to understand and concise.
-      
-      My primary tools are JavaScript and React, which I use to create visually appealing and easy-to-navigate front-end web applications. My objective is to continuously improve my skills in these areas and produce interfaces that delight users.
-      
-      Ultimately, I aim to continue honing my programming skills and eventually work as a developer. I am thrilled to be part of the tech community and excited to contribute to the development of innovative solutions that can make people's lives easier and more enjoyable.`,
+    presentation: `Combining a background in ESL teaching with recent experience in QA testing, I'm now immersed in a Data Science university program. I have a passion for technology and a commitment to mastering Statistics, Python, and data analysis. Before deciding to pursue Data Science, I was studying front-end development, which provided me with a solid foundation in JavaScript and a good understanding of SQL, HTML, CSS, and ReactJS. I am always ready to face new challenges. I am always ready to face new challenges and commit to reading books, meeting peers, taking courses, and engaging myself in anything data-related.`,
   },
   {
-    title: `Olá, eu sou a Emily e estou me tornando uma desenvolvedora Front-end.`, 
-    
-    presentation: `Comecei a aprender programação em 2022 e, desde então, estou fascinada pelas infinitas possibilidades criativas que a programação oferece. Atualmente, estou na faculdade para aprimorar minhas habilidades e conhecimentos nessa área.
+    title: `Olá, me chamo Emily e pretendo me tornar uma Cientista de Dados`,
 
-             Além da minha paixão pela programação, também sou professora de inglês. Adoro ensinar e ajudar os outros a aprender, e acredito que essa habilidade será valiosa na minha carreira como desenvolvedora. A comunicação é fundamental tanto no ensino quanto na programação, e faço questão de escrever um código claro, conciso e compreensível.
-
-             Utilizo HTML, CSS, JavaScript e React para criar interfaces atraentes e de fácil uso em aplicações web. Meu objetivo é continuar aprimorando minhas habilidades nessas áreas, desenvolvendo interfaces que cativem os usuários e estar aberta a novas tecnologias.
-
-             Quero continuar aprimorando minhas habilidades de programação e trabalhar como desenvolvedora. Estou muito animada para fazer parte da comunidade de tecnologia e contribuir para o desenvolvimento de soluções inovadoras que possam facilitar e tornar a vida das pessoas mais agradável.`,
+    presentation: `Desde minha experiência como professora de inglês até minha experiência recente como QA tester, agora tenho meu foco no curso de Ciência de Dados. Tenho uma paixão pela tecnologia e estou compromissada em dominar Estatística, Python e análise de dados. Antes de decidir seguir a área de Ciência de Dados, estava estudando desenvolvimento front-end, o que me concedeu uma base sólida em JavaScript e uma ótima noção de SQL, HTML, CSS e ReactJS. Sempre pronta para aceitar novos desafios, eu me disponho a ler livros, fazer networking, fazer cursos e respirar em qualquer coisa relacionada a dados.`,
   },
   {
-    title: `Hola, soy Emily y estoy en proceso de convertirme en desarrolladora de front-end.`, 
-    
-    presentation: `Empecé a aprender programación en 2022 y desde entonces estoy fascinada por las infinitas posibilidades creativas que ofrece la programación. Actualmente, estoy en la universidad para mejorar mis habilidades y conocimientos en esta área.
+    title: `Hola, soy Emily y quiero ser una Científica de Datos`,
 
-        Además de mi pasión por la programación, también soy profesora de inglés. Me encanta enseñar y ayudar a otros a aprender, y creo que esta habilidad será valiosa en mi futuro como desarrolladora. La comunicación es clave tanto en la enseñanza como en la programación, y me esfuerzo por escribir un código claro, conciso y comprensible.
-        
-        Utilizo JavaScript y React para crear interfaces atractivas y fáciles de usar en aplicaciones web. Mi objetivo es seguir mejorando mis habilidades en estas áreas y desarrollar interfaces que conquisten a los usuarios.
-        
-        Quiero seguir mejorando mis habilidades de programación y trabajar como desarrolladora. Estoy muy emocionada de formar parte de la comunidad de tecnología y contribuir al desarrollo de soluciones innovadoras que puedan hacer la vida de las personas más fácil y agradable.`,
+    presentation: `Desde mi experiencia como profesora de inglés hasta mi reciente experiencia como QA tester, ahora mi enfoque está en la universidad de Ciencia de Datos. Me apasiona la tecnología y estoy comprometida a dominar Estadística, Python y análisis de datos. Antes de decidirme por la Ciencia de Datos, estaba estudiando desarrollo front-end, lo que me otorgó una base sólida en JavaScript y un buen entendimiento de SQL, HTML, CSS y ReactJS. Siempre lista para aceptar nuevos desafíos, me dispongo a leer libros, hacer networking, tomar cursos y sumergirme en todo lo relacionado con los datos.`,
   },
   {
-    title: `Bonjour, je m'appelle Emily et je suis en train de devenir développeuse front-end.`, 
-    
-    presentation: `J'ai commencé à apprendre la programmation en 2022 et depuis, je suis fascinée par les possibilités créatives que le codage offre. Actuellement, je suis étudiante à l'université pour approfondir mes compétences et mes connaissances dans ce domaine.
+    title: `Bonjour, je suis Emily, aspirante Data Scientist`,
 
-        En plus de ma passion pour le codage, je suis également professeure d'anglais. J'adore enseigner et aider les autres à apprendre, et je pense que cette compétence sera précieuse dans mon avenir de développeuse. La communication est essentielle tant dans l'enseignement que dans la programmation, et je m'efforce de rendre mon code clair, concis et compréhensible.
-        
-        J'utilise JavaScript et React pour créer des applications web frontales. Mon objectif est d'améliorer continuellement mes compétences dans ces domaines et de développer des interfaces visuellement attrayantes et faciles à naviguer.
-        
-        Je souhaite continuer à améliorer mes compétences en programmation et travailler à terme en tant que développeuse. Je suis ravie de faire partie de la communauté technologique et de contribuer au développement de solutions innovantes qui peuvent faciliter et améliorer la vie des gens.`,
+    presentation: `Depuis mon expérience dans l'enseignement de l'anglais jusqu'à mon poste récent en QA testing, je mets désormais le cap sur le cours de Data Science. J'ai une passion pour la technologie et je m'engage à maîtriser la statistique, Python et l'analyse de Data. Avant de m'orienter vers la Data Science, j'étudiais le développement front-end, ce qui m'a donné une base solide en JavaScript une excellente maîtrise de SQL, HTML, CSS et ReactJS. Toujours prête à relever de nouveaux défis, je suis avide de lectures, de réseautage, à suivre des cours et à me plonger dans tout ce qui concerne le Data.`,
   },
 ];
 
